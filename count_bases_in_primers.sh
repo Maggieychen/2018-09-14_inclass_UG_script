d1dcf0d6d2fad9986aca7f79c335301942a66c47
Maggie Chen <ychen254@dons.usfca.edu>
2018-09-14 15:16:25 -0700

    Add in for loop to handle multiple files

--- count_bases_in_primers.sh
+++ count_bases_in_primers.sh
@@ -17,4 +17,7 @@
 # sign.
 # Another slolution would be to use tr like so
 # head -3 primer_B.fasta | tail -1 | tr -d "\n" |wc -m
-expr $(head -n 3 "$@" | tail -1 |wc -m) - 1
+for file in "$@"
+do
+   expr $(head -n 3 "$@" | tail -1 |wc -m) - 1
+done
